956794a5981b100e8cac3cc89bb71f292e435f2c
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-03-30 20:47:39 +0900

    [update] server.go

--- cmd/server.go
+++ cmd/server.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/Tomoya113/go-mvc-api-server/pkg/database"
 )
 
 func main() {
 	r := InitializeRouter()
+	error := database.Init(nil)
+	if error != nil {
+		panic(error)
+	}
 	http.ListenAndServe(":3333", r)
 }
