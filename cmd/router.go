fd27fabdcff605243526fac2af4011ad14972da5
Tomoya Tanaka <tomoya113.code@gmail.com>
2022-04-08 21:26:39 +0900

    [update] router

--- cmd/router.go
+++ cmd/router.go
@@ -1,24 +1,18 @@
 package main
 
 import (
+	"github.com/Tomoya113/go-mvc-api-server/pkg/controller"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/Tomoya113/go-mvc-api-server/pkg/controller"
-	"github.com/Tomoya113/go-mvc-api-server/pkg/model"
-	"github.com/Tomoya113/go-mvc-api-server/pkg/view"
 )
 
 func InitializeRouter() chi.Router {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	v := view.NewUserView()
-	m := model.NewUserModel()
-	c := controller.NewUserController(v, m)
-
 	r.Route("/users", func(r chi.Router) {
-		r.Get("/", c.GetUsers)
-		r.Post("/", c.CreateUser)
+		r.Get("/", controller.GetUsers)
+		r.Post("/", controller.CreateUser)
 	})
 
 	return r
